04dde133ec27a9be2dd04129b72a462c7dc3c38b
Łukasz Pięta <pieta.lukasz98@gmail.com>
2021-02-03 20:48:16 +0100

    Logging some random metrics

--- black-box/src/main/scala/CustomListener.scala
+++ black-box/src/main/scala/CustomListener.scala
@@ -4,9 +4,45 @@ import org.apache.spark.scheduler._
 class CustomListener extends SparkListener {
   private val logger = LogManager.getLogger("CustomListener")
 
-  override def onApplicationEnd(applicationEnd: SparkListenerApplicationEnd): Unit = super.onApplicationEnd(applicationEnd)
+  override def onJobStart(jobStart: SparkListenerJobStart): Unit = {
+    logger.warn(s"Job Id: ${jobStart.jobId}")
+    logger.warn(s"Job properties: ${jobStart.properties}")
+    logger.warn(s"Job stage ids: ")
+    jobStart.stageIds.foreach(stageId =>
+      logger.warn(s"${stageId}")
+    )
+  }
+
+
+  override def onJobEnd(jobEnd: SparkListenerJobEnd): Unit = {
+    logger.warn(s"Job Id: ${jobEnd.jobId}")
+    logger.warn(s"Job result: ${jobEnd.jobResult}")
+    logger.warn(s"Job time: ${jobEnd.time}")
+
+  }
+
+  override def onApplicationStart(applicationStart: SparkListenerApplicationStart): Unit = {
+    logger.warn(s"Application time: ${applicationStart.time}")
+    logger.warn(s"Application driver logs: ${applicationStart.driverLogs}")
+  }
+
+  override def onApplicationEnd(applicationEnd: SparkListenerApplicationEnd): Unit = {
+    logger.warn(s"Application time: ${applicationEnd.time}")
+  }
+
+  override def onTaskEnd(taskEnd: SparkListenerTaskEnd): Unit = {
+    logger.warn(s"TaskId: ${taskEnd.taskInfo.taskId}, resultSize: ${taskEnd.taskMetrics.resultSize}")
+  }
 
   override def onStageCompleted(stageCompleted: SparkListenerStageCompleted): Unit = {
+    logger.warn("ParentIds: ")
+    stageCompleted.stageInfo.parentIds.foreach(logger.warn(_))
+
+    logger.warn(stageCompleted.stageInfo.details)
+
+    logger.warn(s"Bytes read: ${stageCompleted.stageInfo.taskMetrics.inputMetrics.bytesRead} ")
+    logger.warn(s"Bytes written: ${stageCompleted.stageInfo.taskMetrics.outputMetrics.bytesWritten} ")
+
     logger.warn(s"Stage completed, runTime: ${stageCompleted.stageInfo.taskMetrics.executorRunTime}, " +
       s"cpuTime: ${stageCompleted.stageInfo.taskMetrics.executorCpuTime}")
 
