a830229ba917937e218d80d734fb9d38a1c683f8
Łukasz Pięta <pieta.lukasz98@gmail.com>
2021-02-22 12:44:45 +0100

    Delete onJobStart method

--- black-box/src/main/scala/CustomListener.scala
+++ black-box/src/main/scala/CustomListener.scala
@@ -4,15 +4,6 @@ import org.apache.spark.scheduler._
 class CustomListener extends SparkListener {
   private val logger = LogManager.getLogger("CustomListener")
 
-  override def onJobStart(jobStart: SparkListenerJobStart): Unit = {
-    logger.warn(s"Job Id: ${jobStart.jobId} STARTING")
-    logger.warn(s"Job properties: ${jobStart.properties}")
-    logger.warn(s"Job stage ids: ")
-    jobStart.stageIds.foreach(stageId =>
-      logger.warn(s"${stageId}")
-    )
-  }
-
   override def onStageCompleted(stageCompleted: SparkListenerStageCompleted): Unit = {
     new JdbcConnect().connect().insert(stageCompleted.stageInfo).close()
   }
