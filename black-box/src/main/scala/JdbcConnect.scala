849744fd838f60049863ce19308040aaa53ce498
Łukasz Pięta <pieta.lukasz98@gmail.com>
2021-02-28 19:16:58 +0100

    Save to db onTaskEnd

--- black-box/src/main/scala/JdbcConnect.scala
+++ black-box/src/main/scala/JdbcConnect.scala
@@ -1,4 +1,5 @@
 import org.apache.log4j.LogManager
+import org.apache.spark.executor.TaskMetrics
 import org.apache.spark.scheduler.StageInfo
 
 import java.sql.{Connection, DriverManager, Statement}
@@ -21,11 +22,23 @@ class JdbcConnect {
     this
   }
 
+  def insert(taskType: String, stageId: Int, taskMetrics: TaskMetrics): JdbcConnect = {
+    val sql = s"insert into task_metrics(function_name, stage_id, task_type," +
+      s"executor_run_time, result_size, jvm_gc_time, peak_execution_memory, disk_bytes_spilled, memory_bytes_spilled) " +
+      s"values('${BlackBox.functionName}', $stageId, '$taskType', ${taskMetrics.executorRunTime}, ${taskMetrics.resultSize}, " +
+      s"${taskMetrics.jvmGCTime}, ${taskMetrics.peakExecutionMemory}, ${taskMetrics.diskBytesSpilled}, ${taskMetrics.memoryBytesSpilled})"
+
+    logger.warn(sql)
+    statement.execute(sql)
+
+    this
+  }
+
   def insert(stageInfo: StageInfo): JdbcConnect = {
-    val sql = s"insert into metrics(function_name, stage_id, details, num_tasks, name, " +
+    val sql = s"insert into stage_metrics(function_name, stage_id, num_tasks, " +
       s"submission_time, completion_time, executor_run_time, result_size, jvm_gc_time, peak_execution_memory, disk_bytes_spilled, memory_bytes_spilled) " +
-      s"values('${BlackBox.functionName}', ${stageInfo.stageId}, '${stageInfo.details}', ${stageInfo.numTasks}, " +
-      s"'${stageInfo.name}', ${stageInfo.submissionTime.get}, ${stageInfo.completionTime.get}, ${stageInfo.taskMetrics.executorRunTime}, ${stageInfo.taskMetrics.resultSize}, " +
+      s"values('${BlackBox.functionName}', ${stageInfo.stageId}, ${stageInfo.numTasks}, " +
+      s"${stageInfo.submissionTime.get}, ${stageInfo.completionTime.get}, ${stageInfo.taskMetrics.executorRunTime}, ${stageInfo.taskMetrics.resultSize}, " +
       s"${stageInfo.taskMetrics.jvmGCTime}, ${stageInfo.taskMetrics.peakExecutionMemory}, ${stageInfo.taskMetrics.diskBytesSpilled}, ${stageInfo.taskMetrics.memoryBytesSpilled})"
 
     logger.warn(sql)
