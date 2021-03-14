e3088f8aa39256dce592cbb30c134f8a0b7e3602
Łukasz Pięta <pieta.lukasz98@gmail.com>
2021-03-14 16:13:51 +0100

    Configure database - fixes

---
+++ database/README.md
@@ -0,0 +1,8 @@
+# Run database
+
+## Get data
+
+```
+chmod 755 get_data.sh
+./get_data.sh
+```
--- database/run_database.sh
+++ database/run_database.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 docker-compose up -d
+
+wait 5
+
+docker network connect --ip 10.5.0.8 spark-network postgres
