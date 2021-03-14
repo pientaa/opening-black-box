646ccdd0775d74537f1810a3553bf173c198d1a6
Łukasz Pięta <pieta.lukasz98@gmail.com>
2021-03-14 18:12:53 +0100

    Rollback sumbit.sh

--- black-box/submit.sh
+++ black-box/submit.sh
@@ -4,7 +4,7 @@ sbt assembly
 
 sudo cp ./target/scala-2.11/black-box-assembly-1.0.jar ./../spark-config/mnt/spark-apps/black-box-assembly-1.0.jar
 
-curl -X POST http://10.5.0.2:6066/v1/submissions/create --header "Content-Type:application/json;charset=UTF-8" --data '{
+curl -X POST http://localhost:6066/v1/submissions/create --header "Content-Type:application/json;charset=UTF-8" --data '{
   "appResource": "/opt/spark-apps/black-box-assembly-1.0.jar",
   "sparkProperties": {
     "spark.master": "spark://spark-master:7077",
@@ -21,7 +21,7 @@ curl -X POST http://10.5.0.2:6066/v1/submissions/create --header "Content-Type:a
   },
   "action": "CreateSubmissionRequest",
   "appArgs": [
-    "10.5.0.8",
+    "postgres",
     "averageTemperatureByDeviceIdSeason"
   ]
 }'
