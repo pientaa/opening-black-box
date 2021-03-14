bde0924f80a867465b7d83bb0687f41214ee239b
Łukasz Pięta <pieta.lukasz98@gmail.com>
2021-03-14 16:42:11 +0100

    Change localhost to spark-master

--- black-box/submit.sh
+++ black-box/submit.sh
@@ -4,7 +4,7 @@ sbt assembly
 
 sudo cp ./target/scala-2.11/black-box-assembly-1.0.jar ./../spark-config/mnt/spark-apps/black-box-assembly-1.0.jar
 
-curl -X POST http://localhost:6066/v1/submissions/create --header "Content-Type:application/json;charset=UTF-8" --data '{
+curl -X POST http://spark-master:6066/v1/submissions/create --header "Content-Type:application/json;charset=UTF-8" --data '{
   "appResource": "/opt/spark-apps/black-box-assembly-1.0.jar",
   "sparkProperties": {
     "spark.master": "spark://spark-master:7077",
