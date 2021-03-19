3f736ad659f75e04a3ea136b0f87866a11a88fe1
Łukasz Pięta <pieta.lukasz98@gmail.com>
2021-03-19 18:27:49 +0100

    Update get_data.sh to be run remote only

--- database/get_data.sh
+++ database/get_data.sh
@@ -1,1 +1,9 @@
-wget https://gitlab.com/pientaa/black-box-data/-/raw/master/measurements.csv
+#!/bin/bash
+#Change this directory if want to run locally
+filename=~/opening-black-box/database/measurements.csv
+
+if [ ! -f $filename ]
+then
+  echo 'Getting data to file: ' $filename
+  wget https://gitlab.com/pientaa/black-box-data/-/raw/master/measurements.csv
+fi
