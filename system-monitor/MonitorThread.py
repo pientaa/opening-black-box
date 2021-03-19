a192437b6c7480ef63b16593c382c5a39879fe5b
Bertt-hub <hubert.krzyzanowski@student.put.poznan.pl>
2021-03-19 15:46:41 +0100

    Working monitoring top | tail | awk

--- system-monitor/MonitorThread.py
+++ system-monitor/MonitorThread.py
@@ -1,6 +1,6 @@
 import threading
 import time
-import subprocess
+from subprocess import Popen, PIPE
 
 class MonitorThread(threading.Thread):
 
@@ -19,25 +19,39 @@ class MonitorThread(threading.Thread):
         self.pids = pids
 
     def run(self):
-        top_cmd = ['top','-b', '-p', self.pids]
+        top_cmd = ['top','-b','-n 1', '-p', self.pids]
+        tail_cmd = ['tail', '-n+8']
+        # PID CPU RAM
+        awk_cmd = ['awk', '{print $1 "\t" $9 "\t" $10}']
         
-        top_process = subprocess.Popen(top_cmd, stdout=subprocess.PIPE)
-        tail_process = subprocess.Popen()
-
         while True:
-            if self.stopped():
-                print("TH | Thread stopping!")
-                top_process.stdout.close()
-                top_process.kill()
-                top_process.wait()
-                return
+            top_process = Popen(top_cmd, stdout=PIPE)
+            tail_process = Popen(tail_cmd, stdin=top_process.stdout, stdout=PIPE)
+            awk_process = Popen(awk_cmd, stdin=tail_process.stdout, stdout=PIPE)
 
-            output = top_process.stdout.readline()
-            if output == '' and top_process.poll() is not None:
-                break
+            output = awk_process.communicate()[0]
+            output = output.decode()
 
             if output:
+                print(time.time())
+                output = output.split("\n")
                 print(output)
+
+            top_process.stdout.close()
+            top_process.kill()
+            top_process.wait()
+
+            tail_process.stdout.close()
+            tail_process.kill()
+            tail_process.wait()
+
+            awk_process.stdout.close()
+            awk_process.kill()
+            awk_process.wait()
+
+            if self.stopped():
+                print("TH | Thread stopping!")
+                return
 
         return
  
