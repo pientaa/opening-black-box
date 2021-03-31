151e97d3ee073d0fe1fb231fd29f3ef59c0d0d86
Łukasz Pięta <pieta.lukasz98@gmail.com>
2021-03-31 13:31:33 +0200

    Two separate submits -> one on local, second on cluster

---
+++ sumbit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+functionName=${1:-averageTemperatureByDeviceIdSeason}
+# Get driver status
+#docker exec -it spark-master curl http://10.5.0.2:6066/v1/submissions/status/<driver-id>
+
+docker exec -it spark-master curl -X POST http://10.5.0.2:6066/v1/submissions/create --header $header --data '{
+  "appResource": "/opt/spark-apps/black-box-assembly-1.0.jar",
+  "sparkProperties": {
+    "spark.master": "spark://spark-master:7077",
+    "spark.driver.memory": "4g",
+    "spark.driver.cores": "2",
+    "spark.app.name": "BlackBox",
+    "spark.submit.deployMode": "cluster",
+    "spark.driver.supervise": "true"
+  },
+  "clientSparkVersion": "2.4.7",
+  "mainClass": "BlackBox",
+  "environmentVariables": {
+    "SPARK_ENV_LOADED": "1"
+  },
+  "action": "CreateSubmissionRequest",
+  "appArgs": [
+    "postgres",
+    "'$functionName'"
+  ]
+}'
