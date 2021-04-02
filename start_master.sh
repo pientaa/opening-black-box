52849846cc6e16a0f8e83a7e8ef0fa49ab7523f6
Łukasz Pięta <pieta.lukasz98@gmail.com>
2021-04-02 15:23:03 +0200

    No gateway, create bridge network

--- start_master.sh
+++ start_master.sh
@@ -2,7 +2,7 @@
 
 echo 'Init docker swarm, create spark-network, run spark master and set up database'
 sshpass -f "password.env" ssh 20 "docker swarm init;"
-sshpass -f "password.env" ssh 20 "docker network create -d overlay --attachable --ipam-driver=default --subnet=10.5.0.0/16 --gateway=192.168.55.20 spark-network;"
+sshpass -f "password.env" ssh 20 "docker network create -d overlay --attachable --ipam-driver=default --subnet=10.5.0.0/16 spark-network;"
 sshpass -f "password.env" ssh 20 "docker-compose -f  ~/opening-black-box/spark-config/spark-master.yml up -d;"
 sshpass -f "password.env" ssh 20 "~/opening-black-box/database/get_data.sh;"
 sshpass -f "password.env" ssh 20 "docker-compose -f  ~/opening-black-box/database/docker-compose.yml up -d;"
