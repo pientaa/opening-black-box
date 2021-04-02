d9d64bb2ce133f3521b848b443075a16c8fb62e2
Łukasz Pięta <pieta.lukasz98@gmail.com>
2021-04-02 13:52:11 +0200

    Don't create network in bash script

--- start_master.sh
+++ start_master.sh
@@ -3,7 +3,6 @@
 echo 'Init docker swarm, create spark-network, run spark master and set up database'
 sshpass -f "password.env" ssh 20 "docker swarm init;"
 sshpass -f "password.env" ssh 20 "docker network create -d overlay --attachable --ipam-driver=default --subnet=10.5.0.0/16 spark-network;"
-sshpass -f "password.env" ssh 20 "docker network create -d bridge --ipam-driver=default --subnet=10.6.0.0/16 spark-network-bridge;"
 sshpass -f "password.env" ssh 20 "docker-compose -f  ~/opening-black-box/spark-config/spark-master.yml up -d;"
 sshpass -f "password.env" ssh 20 "~/opening-black-box/database/get_data.sh;"
 sshpass -f "password.env" ssh 20 "docker-compose -f  ~/opening-black-box/database/docker-compose.yml up -d;"
