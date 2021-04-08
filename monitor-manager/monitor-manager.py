5a60221616fd59f89c80b8b07461ec954c1950b0
Bertt-hub <hubert.krzyzanowski@student.put.poznan.pl>
2021-04-08 15:05:22 +0200

    Update manager

--- monitor-manager/monitor-manager.py
+++ monitor-manager/monitor-manager.py
@@ -12,25 +12,24 @@ app = Flask(__name__)
 def start_experiments():
     experiments_plan = read_csv("experiments-plan.csv")
     hosts_info = read_csv("hosts-info.csv")
-    print("done")
-    # is_spark_node, host_ip = check_if_spark_node(hosts_info)
+    is_spark_node, host_ip = check_if_spark_node(hosts_info)
 
-    # for function_name, table in experiments_plan:
-    #     if is_spark_node:
-    #         submit_response = requests.post("192.168.55.20:5000/submit", json=json.dumps({"function_name": function_name}))
-    #         submit_response = json.loads(submit_response.text)
-    #         driver_id = submit_response["submissionId"]
+    for function_name, table in experiments_plan:
+        if is_spark_node:
+            submit_response = requests.post("192.168.55.20:5000/submit", json=json.dumps({"function_name": function_name}))
+            submit_response = json.loads(submit_response.text)
+            driver_id = submit_response["submissionId"]
 
-    #         system_monitor(host_ip, hosts_info, True)
+            system_monitor(host_ip, hosts_info, True)
 
-    #         status = "RUNNING"
-    #         while status == "RUNNING":
-    #             status_response = requests.get("192.168.55.20:5000/status", json=json.dumps({"driver_id": driver_id}))
-    #             status_response = json.loads(status_response.text)
-    #             status = status_response["driverState"]
-    #             time.sleep(1)
+            status = "RUNNING"
+            while status == "RUNNING":
+                status_response = requests.get("192.168.55.20:5000/status", json=json.dumps({"driver_id": driver_id}))
+                status_response = json.loads(status_response.text)
+                status = status_response["driverState"]
+                time.sleep(1)
 
-    #         system_monitor(host_ip, hosts_info, False)
+            system_monitor(host_ip, hosts_info, False)
             
 def system_monitor(host_ip, hosts_info, start):
     for ip, container_name in hosts_info:
@@ -61,5 +60,4 @@ def check_if_spark_node(hosts_info):
     return False, ip_address    
 
 if __name__ == "__main__":
-    # app.run(debug=True, host='0.0.0.0', port=8888)
-    start_experiments()
+    app.run(debug=True, host='0.0.0.0', port=8888)
