11e354bfaec4844e1cb41b86d0f67f287609ffc9
Bertt-hub <hubert.krzyzanowski@student.put.poznan.pl>
2021-04-28 09:53:42 +0200

    Configuring TPC-DS tools on cluster Generating TPC-DS data on cluster

--- database/generate_data.sh
+++ database/generate_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-data_size=${1:-1}
 
-cd ~opening-black-box/database/tpc-ds/v3.0.0/tools
-./dsdgen -SCALE $data_szie -DIR ./../../../data
+data_size=${1:-1}
+directory_name="${data_size}GB"
+
+sshpass -f "./scripts/password.env" ssh 20 "cd ~/opening-black-box/database/tpc-data ; mkdir $directory_name "
+sshpass -f "./scripts/password.env" ssh 20 "cd ~/opening-black-box/database/tpc-ds/v3.0.0/tools ; ./dsdgen -SCALE $data_size -DIR ./../../../tpc-data/$directory_name"
